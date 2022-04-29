a20bb8d4384ebd17f158891ff13447593d293ffe
Luljana Selmanoski <lselmanoski@gmx.de>
2022-04-29 15:54:40 +0200

    Add mongoDB

--- pages/api/card/[id].js
+++ pages/api/card/[id].js
@@ -1,18 +1,24 @@
+import { Card } from "../../../src/model/Card";
+
 import { getCards } from "../../../src/services/get-cards";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
   const { id } = request.query;
 
-  const cards = getCards();
+  //const cards = getCards();
 
-  const singleCard = cards.find((card) => card.id === id);
+  //const singleCard = cards.find((card) => card.id === id);
 
   if (request.method === "DELETE") {
-    response.status(200).json({ message: "card deleted", card: singleCard });
+    const deleted = await Card.findByIdAndDelete(id);
+    response.status(200).json({ message: "card deleted", card: deletedCard });
   } else if (request.method === "PUT") {
-    const changedCard = JSON.parse(request.body);
+    const data = JSON.parse(request.body);
+
+    const changedCard = await Card.findByIdAndDelete(id, data, { new: true });
     response.status(200).json({ message: "card updated", card: changedCard });
   } else {
+    const singleCard = await Card.findById(id);
     response.status(200).json(singleCard);
   }
 }
--- src/services/get-cards.js
+++ src/services/get-cards.js
@@ -1,5 +1,14 @@
-import data from "./static-cards.json";
+import mongoose from "mongoose";
+import { dbConnect } from "../lib/database";
+import Card from "../model/Card";
 
-export const getCards = () => {
-  return data;
+export const getCards = async () => {
+  await dbConnect();
+  const data = await Card.find().populate("user");
+  console.log(data);
+  return data.map(({ id, content, user }) => ({
+    id,
+    content,
+    name: user.name,
+  }));
 };
