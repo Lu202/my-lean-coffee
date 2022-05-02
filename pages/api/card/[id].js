500e926894cd3e6606f1b3efc174136046e2623f
Luljana Selmanoski <lselmanoski@gmx.de>
2022-05-02 16:56:03 +0200

    Add _update

--- pages/api/card/[id].js
+++ pages/api/card/[id].js
@@ -1,4 +1,5 @@
-import { Card } from "../../../src/model/Card";
+import Card from "../../../src/model/Card";
+import User from "../../../src/model/User";
 
 import { getCards } from "../../../src/services/get-cards";
 
@@ -14,9 +15,22 @@ export default async function handler(request, response) {
     response.status(200).json({ message: "card deleted", card: deletedCard });
   } else if (request.method === "PUT") {
     const data = JSON.parse(request.body);
+    const card = await Card.findById(id);
+    const changedCard = await Card.findByIdAndUpdate(
+      id,
+      { content: data.content },
+      { new: true }
+    );
+    const changeUser = await Card.findByIdAndUpdate(
+      id,
+      { content: data.content },
+      { new: true }
+    );
 
-    const changedCard = await Card.findByIdAndDelete(id, data, { new: true });
-    response.status(200).json({ message: "card updated", card: changedCard });
+    //const changedCard = await Card.findByIdAndDelete(id, data, { new: true });
+    response
+      .status(200)
+      .json({ message: "card updated", card: changedCard, card: changeUser });
   } else {
     const singleCard = await Card.findById(id);
     response.status(200).json(singleCard);
